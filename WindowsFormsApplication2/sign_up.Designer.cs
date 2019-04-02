053c9eae1972ff298dcad4600ca0b4dc72596689
AdelSarwat <AdelSarwat@users.noreply.github.com>
2019-04-02 18:39:11 +0200

    first commit

---
+++ WindowsFormsApplication2/sign_up.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class sign_up
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_username = new ZBobb.AlphaBlendTextBox();
+            this.txt_con_pass = new ZBobb.AlphaBlendTextBox();
+            this.txt_pass = new ZBobb.AlphaBlendTextBox();
+            this.txt_name = new ZBobb.AlphaBlendTextBox();
+            this.btn_signup = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txt_passport_num = new ZBobb.AlphaBlendTextBox();
+            this.txt_gmail = new ZBobb.AlphaBlendTextBox();
+            this.txt_gmail_pass = new ZBobb.AlphaBlendTextBox();
+            this.SuspendLayout();
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackAlpha = 50;
+            this.txt_username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_username.Location = new System.Drawing.Point(169, 148);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_username.MaxLength = 30;
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(251, 32);
+            this.txt_username.TabIndex = 27;
+            this.txt_username.Text = "UserName";
+            this.txt_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_username.Enter += new System.EventHandler(this.textboxusername1_Enter);
+            this.txt_username.Leave += new System.EventHandler(this.textboxusername1_Leave);
+            // 
+            // txt_con_pass
+            // 
+            this.txt_con_pass.BackAlpha = 50;
+            this.txt_con_pass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_con_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_con_pass.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_con_pass.Location = new System.Drawing.Point(169, 220);
+            this.txt_con_pass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_con_pass.MaxLength = 30;
+            this.txt_con_pass.Name = "txt_con_pass";
+            this.txt_con_pass.Size = new System.Drawing.Size(251, 32);
+            this.txt_con_pass.TabIndex = 26;
+            this.txt_con_pass.Text = "Confirm Password";
+            this.txt_con_pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_con_pass.Enter += new System.EventHandler(this.textboxconfirmpassword_Enter);
+            this.txt_con_pass.Leave += new System.EventHandler(this.textboxconfirmpassword_Leave);
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.BackAlpha = 50;
+            this.txt_pass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pass.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_pass.Location = new System.Drawing.Point(169, 184);
+            this.txt_pass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_pass.MaxLength = 30;
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(251, 32);
+            this.txt_pass.TabIndex = 25;
+            this.txt_pass.Text = "Password";
+            this.txt_pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_pass.Enter += new System.EventHandler(this.textboxpassword1_Enter);
+            this.txt_pass.Leave += new System.EventHandler(this.textboxpassword1_Leave);
+            // 
+            // txt_name
+            // 
+            this.txt_name.BackAlpha = 50;
+            this.txt_name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_name.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_name.Location = new System.Drawing.Point(169, 112);
+            this.txt_name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_name.MaxLength = 30;
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(251, 32);
+            this.txt_name.TabIndex = 24;
+            this.txt_name.Text = "Name";
+            this.txt_name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_name.Enter += new System.EventHandler(this.textboxname_Enter);
+            this.txt_name.Leave += new System.EventHandler(this.textboxname_Leave);
+            // 
+            // btn_signup
+            // 
+            this.btn_signup.BackColor = System.Drawing.Color.White;
+            this.btn_signup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_signup.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_signup.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.btn_signup.Location = new System.Drawing.Point(224, 425);
+            this.btn_signup.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_signup.Name = "btn_signup";
+            this.btn_signup.Size = new System.Drawing.Size(134, 41);
+            this.btn_signup.TabIndex = 23;
+            this.btn_signup.Text = "Sign up";
+            this.btn_signup.UseVisualStyleBackColor = false;
+            this.btn_signup.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackgroundImage = global::WindowsFormsApplication2.Properties.Resources.exit1;
+            this.btn_exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Location = new System.Drawing.Point(718, -1);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(40, 40);
+            this.btn_exit.TabIndex = 32;
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::WindowsFormsApplication2.Properties.Resources.exit1;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(515, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 40);
+            this.button3.TabIndex = 38;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txt_passport_num
+            // 
+            this.txt_passport_num.BackAlpha = 50;
+            this.txt_passport_num.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_passport_num.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_passport_num.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_passport_num.Location = new System.Drawing.Point(169, 256);
+            this.txt_passport_num.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_passport_num.MaxLength = 30;
+            this.txt_passport_num.Name = "txt_passport_num";
+            this.txt_passport_num.Size = new System.Drawing.Size(251, 32);
+            this.txt_passport_num.TabIndex = 39;
+            this.txt_passport_num.Text = "Passport number";
+            this.txt_passport_num.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_passport_num.TextChanged += new System.EventHandler(this.txt_passport_num_TextChanged);
+            this.txt_passport_num.Enter += new System.EventHandler(this.txt_passport_num_Enter);
+            this.txt_passport_num.Leave += new System.EventHandler(this.txt_passport_num_Leave);
+            // 
+            // txt_gmail
+            // 
+            this.txt_gmail.BackAlpha = 50;
+            this.txt_gmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_gmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_gmail.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_gmail.Location = new System.Drawing.Point(169, 292);
+            this.txt_gmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_gmail.MaxLength = 30;
+            this.txt_gmail.Name = "txt_gmail";
+            this.txt_gmail.Size = new System.Drawing.Size(251, 32);
+            this.txt_gmail.TabIndex = 40;
+            this.txt_gmail.Text = "gmail";
+            this.txt_gmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_gmail.Enter += new System.EventHandler(this.txt_gmail_Enter);
+            this.txt_gmail.Leave += new System.EventHandler(this.txt_gmail_Leave);
+            // 
+            // txt_gmail_pass
+            // 
+            this.txt_gmail_pass.BackAlpha = 50;
+            this.txt_gmail_pass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_gmail_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_gmail_pass.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txt_gmail_pass.Location = new System.Drawing.Point(169, 328);
+            this.txt_gmail_pass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_gmail_pass.MaxLength = 30;
+            this.txt_gmail_pass.Name = "txt_gmail_pass";
+            this.txt_gmail_pass.Size = new System.Drawing.Size(251, 32);
+            this.txt_gmail_pass.TabIndex = 41;
+            this.txt_gmail_pass.Text = "gmail password";
+            this.txt_gmail_pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_gmail_pass.Enter += new System.EventHandler(this.txt_gmail_pass_Enter);
+            this.txt_gmail_pass.Leave += new System.EventHandler(this.txt_gmail_pass_Leave);
+            // 
+            // sign_up
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication2.Properties.Resources.background21;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(553, 554);
+            this.Controls.Add(this.txt_gmail_pass);
+            this.Controls.Add(this.txt_gmail);
+            this.Controls.Add(this.txt_passport_num);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.txt_con_pass);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.btn_signup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "sign_up";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZBobb.AlphaBlendTextBox txt_username;
+        private ZBobb.AlphaBlendTextBox txt_con_pass;
+        private ZBobb.AlphaBlendTextBox txt_pass;
+        private ZBobb.AlphaBlendTextBox txt_name;
+        private System.Windows.Forms.Button btn_signup;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button button3;
+        private ZBobb.AlphaBlendTextBox txt_passport_num;
+        private ZBobb.AlphaBlendTextBox txt_gmail;
+        private ZBobb.AlphaBlendTextBox txt_gmail_pass;
+    }
+}
---
+++ WindowsFormsApplication2/sign_up.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication2
+{
+    public partial class sign_up : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-NR3KBL6\\ADELSQL;Initial Catalog=airport;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader Rd;
+        public sign_up()
+        {
+            InitializeComponent();
+        }
+        private void textboxname_Enter(object sender, EventArgs e)
+        {
+            if (txt_name.Text == "Name")
+            {
+                txt_name.Text = "";
+            }
+        }
+
+        private void textboxname_Leave(object sender, EventArgs e)
+        {
+            if (txt_name.Text == "")
+            {
+                txt_name.Text = "Name";
+            }
+        }
+
+        private void textboxusername1_Enter(object sender, EventArgs e)
+        {
+            if (txt_username.Text == "UserName")
+            {
+                txt_username.Text = "";
+            }
+        }
+
+        private void textboxusername1_Leave(object sender, EventArgs e)
+        {
+            if (txt_username.Text == "")
+            {
+                txt_username.Text = "UserName";
+            }
+        }
+
+        private void textboxpassword1_Enter(object sender, EventArgs e)
+        {
+            if (txt_pass.Text == "Password")
+            {
+                txt_pass.Text = "";
+                txt_pass.PasswordChar = '*';
+            }
+        }
+
+        private void textboxpassword1_Leave(object sender, EventArgs e)
+        {
+            if (txt_pass.Text == "")
+            {
+                txt_pass.PasswordChar = '\0';
+                txt_pass.Text = "Password";
+            }
+        }
+
+        private void textboxconfirmpassword_Enter(object sender, EventArgs e)
+        {
+            if (txt_con_pass.Text == "Confirm Password")
+            {
+                txt_con_pass.Text = "";
+                txt_con_pass.PasswordChar = '*';
+            }
+        }
+
+        private void textboxconfirmpassword_Leave(object sender, EventArgs e)
+        {
+            if (txt_con_pass.Text == "")
+            {
+                txt_con_pass.Text = "Confirm Password";
+                txt_con_pass.Text = "Password";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(" select * from passenger where username = '" + txt_username.Text + "'", con);
+
+            //  cmd.CommandType = CommandType.Text;
+
+            SqlDataReader Rd = cmd.ExecuteReader();
+
+            bool found_username = false;
+            while (Rd.Read())
+            {
+                found_username = true;
+            }
+            Rd.Close();
+            cmd = new SqlCommand(" select * from passenger where passport = '" + txt_passport_num.Text + "'", con);
+
+            Rd = cmd.ExecuteReader();
+
+            bool found_passport = false;
+            while (Rd.Read())
+            {
+                found_passport = true;
+            }
+            Rd.Close();
+
+            if (txt_con_pass.Text != txt_pass.Text)
+                MessageBox.Show("incorrect password !", "an error occured", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else if (txt_name.Text == "" || txt_pass.Text == "" || txt_name.Text == "" || txt_con_pass.Text == "" || txt_passport_num.Text == "" || txt_gmail.Text =="" || txt_gmail_pass.Text =="")
+                MessageBox.Show("  Please Enter Full Information  ");
+            else if (found_username)
+                MessageBox.Show("this username is already exist try another username");
+            else if (found_passport)
+                MessageBox.Show("this passport is already exist");
+            else
+            {
+                cmd = new SqlCommand(" exec p_signup '" + txt_username.Text + "' ,'" + txt_pass.Text + "','" + txt_name.Text + "','" + txt_passport_num.Text + "','"+txt_gmail.Text+"','"+txt_gmail_pass.Text+ "'", con);
+                cmd.ExecuteNonQuery();
+                // cmd.CommandType = CommandType.StoredProcedure;
+                MessageBox.Show("Done ");
+                p_login f = new p_login();
+                f.Show();
+                this.Hide();
+            }
+            con.Close();
+
+        }
+        
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+
+        private void txt_passport_num_Enter(object sender, EventArgs e)
+        {
+            if (txt_passport_num.Text == "Passport number")
+            {
+                txt_passport_num.Text = "";
+            }
+        }
+
+        private void txt_passport_num_Leave(object sender, EventArgs e)
+        {
+            if (txt_passport_num.Text == "")
+            {
+                txt_passport_num.Text = "Passport number";
+            }
+        }
+
+        private void txt_gmail_Enter(object sender, EventArgs e)
+        {
+            if (txt_gmail.Text == "gmail")
+            {
+                txt_gmail.Text = "";
+            }
+        }
+
+        private void txt_gmail_Leave(object sender, EventArgs e)
+        {
+            if (txt_gmail.Text == "")
+            {
+                txt_gmail.Text = "gmail";
+            }
+        }
+
+        private void txt_gmail_pass_Enter(object sender, EventArgs e)
+        {
+            if (txt_gmail_pass.Text == "gmail password")
+            {
+                txt_gmail_pass.Text = "";
+            }
+        }
+
+        private void txt_gmail_pass_Leave(object sender, EventArgs e)
+        {
+            if (txt_gmail_pass.Text == "")
+            {
+                txt_gmail_pass.Text = "gmail password";
+            }
+        }
+
+        private void txt_passport_num_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txt_passport_num.Text, "[^0-9]"))
+            {
+                MessageBox.Show("please enter only numbers.");
+                txt_passport_num.Text = txt_passport_num.Text.Remove(txt_passport_num.Text.Length - 1);
+            }
+        }
+    }
+
+}
